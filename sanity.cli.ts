f67da247178f96f19e6b84f71afdd92d3baf0cb9
pupontech <50319932+pupontech@users.noreply.github.com>
2026-09-14 14:55:13 +0300

    config: define studioHost in sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,5 +4,6 @@ export default defineCliConfig({
   api: {
     projectId: process.env.PUBLIC_SANITY_PROJECT_ID || '8wvwf5ai',
     dataset: process.env.PUBLIC_SANITY_DATASET || 'production'
-  }
+  },
+  studioHost: 'aygrossphotography'
 });
